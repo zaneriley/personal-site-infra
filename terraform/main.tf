89772670d8474bc1bbafef6c7cc3f0b93c148811
zaneriley <zaneriley@gmail.com>
2024-07-03 23:22:11 +0900

    feat(infrastructure): migrate to dedicated k3s droplet
    - Replace existing droplet with new k3s-specific droplet
    - Update SSH key configuration to use local hostname
    - Simplify Terraform outputs and variables
    - Adjust k3s installation process for new droplet
    
    This change sets up a dedicated droplet for running a k3s Kubernetes
    cluster, improving isolation and management of the infrastructure.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,33 +11,40 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# Existing Droplet
-data "digitalocean_droplet" "existing" {
-  name = "remote-homelab"
+locals {
+  hostname = chomp(file("/proc/sys/kernel/hostname"))
+}
+
+data "digitalocean_ssh_key" "existing" {
+  name = "${local.hostname}"
+}
+
+# Commented out existing Droplet
+# data "digitalocean_droplet" "existing" {
+#   name = "remote-homelab"
+# }
+
+# New Droplet for k3s
+resource "digitalocean_droplet" "k3s_node" {
+  image  = "ubuntu-20-04-x64"
+  name   = "k3s-cluster"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"  
+  ssh_keys = [data.digitalocean_ssh_key.existing.id]
 }
 
 output "droplet_ip" {
-  value = data.digitalocean_droplet.existing.ipv4_address
-}
-
-data "digitalocean_droplets" "all" {}
-
-output "all_droplets" {
-  value = {
-    for droplet in data.digitalocean_droplets.all.droplets :
-    droplet.name => {
-      ip   = droplet.ipv4_address
-      tags = droplet.tags
-    }
-  }
+  value = digitalocean_droplet.k3s_node.ipv4_address
 }
 
 resource "null_resource" "install_k3s" {
+  depends_on = [digitalocean_droplet.k3s_node]
+
   connection {
     type        = "ssh"
     user        = "root"
     private_key = file("~/.ssh/id_rsa")  # Adjust this path as needed
-    host        = data.digitalocean_droplet.existing.ipv4_address
+    host        = digitalocean_droplet.k3s_node.ipv4_address
   }
 
   provisioner "file" {
@@ -54,5 +61,5 @@ resource "null_resource" "install_k3s" {
 }
 
 output "kubeconfig" {
-  value = null_resource.install_k3s.id != "" ? "Run 'ssh root@${data.digitalocean_droplet.existing.ipv4_address} cat /etc/rancher/k3s/k3s.yaml' to get the kubeconfig" : ""
+  value = "Run 'ssh root@${digitalocean_droplet.k3s_node.ipv4_address} cat /etc/rancher/k3s/k3s.yaml' to get the kubeconfig"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,5 @@
 variable "do_token" {
   description = "DigitalOcean API token"
   type        = string
-  default     = ""
 }
 
-variable "ssh_key_fingerprint" {
-  description = "Fingerprint of the SSH key to use for the droplet"
-  type        = string
-  default     = ""
-}
