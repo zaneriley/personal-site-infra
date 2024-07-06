dc2c835c82870526a1883087dfb907c77d37800f
zaneriley <zaneriley@gmail.com>
2024-07-06 10:04:33 +0900

    test script output results

--- scripts/test.sh
+++ scripts/test.sh
@@ -370,6 +370,8 @@ print_summary() {
 }
 
 
+
+# Update the main function
 main() {
     while getopts "vqijf:h" opt; do
         case ${opt} in
@@ -386,6 +388,8 @@ main() {
     if [ ${#targets[@]} -eq 0 ]; then
         targets=($(find . -name "*.yaml" -type f))
     fi
+
+    local tests_run=false
 
     # Run ShellCheck
     run_shellcheck
